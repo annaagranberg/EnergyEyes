1b7165f474b8d30352647e94900d1d7bb6948dce
timol085 <timol085@student.liu.se>
2023-04-03 15:44:21 +0200

    added the fact MUI component

--- src/components/Facts.js
+++ src/components/Facts.js
@@ -1,10 +1,10 @@
+
 import React, { useState } from 'react';
-import energyFacts from '../energyfacts.json';
-import funFacts from '../funfacts.json';
-import refreshIcon from '../refresh.svg';
+import energyFacts from '../data/energyfacts.json';
+import funFacts from '../data/funfacts.json';
 
 function EnergyFacts() {
-  const [fact, setFact] = useState(getRandomFact());
+  const fact = useState(getRandomFact());
 
   function getRandomFact() {
     // Chooses one of the json files at random
@@ -14,38 +14,51 @@ function EnergyFacts() {
     // Selects random number
     return factList[randomIndex].fact;
   }
-
-  // Updates the fact by calling setFact
-  function handleRefreshClick() {
-    setFact(getRandomFact());
-  }
-
+  
   return (
-  //   <div>
-  //     <h5>Energy Fact</h5>
-  //     <p>{fact}</p>
-  //     <img src={refreshIcon} alt="Refresh" onClick={handleRefreshClick} />
-  //   </div>
-  <div style={{ position: 'relative' }}>
-  <div
-    style={{
-      position: 'absolute',
-      top: 10,
-      right: 10,
-      cursor: 'pointer',
-    }}
-    onClick={handleRefreshClick}
-  >
-    <img src={refreshIcon} alt="Refresh" />
-  </div>
-  <h2 style={{ fontSize: '30px' }}>Energy Facts</h2>
-  <p>{fact}</p>
-</div>
+    <p>{fact}</p>
   );
 }
-// icon on button instead
-//<img src={refreshIcon} alt="Refresh" onClick={handleRefreshClick} />
-// text button
-//<button onClick={handleRefreshClick}>Refresh</button>
 
 export default EnergyFacts;
+
+// import React, { useState } from 'react';
+// import energyFacts from '../data/energyfacts.json';
+// import funFacts from '../data/funfacts.json';
+// import { Typography, IconButton } from '@mui/material';
+// import { Refresh } from '@mui/icons-material';
+
+// function FactBox() {
+//   const [fact, setFact] = useState(getRandomFact());
+
+//   function getRandomFact() {
+//     // Chooses one of the json files at random
+//     const factList = Math.random() < 0.7 ? energyFacts.facts : funFacts.facts;
+//     // This line generates a random index value, from 0 to lenght of json file
+//     const randomIndex = Math.floor(Math.random() * factList.length);
+//     // Selects random number
+//     return factList[randomIndex].fact;
+//   }
+
+//   // Updates the fact by calling setFact
+//   function handleRefreshClick() {
+//     setFact(getRandomFact());
+//   }
+
+//   return (
+//     <div style={{ position: 'relative' }}>
+//       <IconButton
+//         style={{ position: 'absolute', top: 10, right: 10 }}
+//         onClick={handleRefreshClick}
+//       >
+//         <Refresh />
+//       </IconButton>
+//       <Typography variant="h4" gutterBottom>
+//         Energy Facts
+//       </Typography>
+//       <Typography variant="body1">{fact}</Typography>
+//     </div>
+//   );
+// }
+
+// export default FactBox;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,12 @@
 import { Box } from '@mui/material'
 import React from 'react'
 import BottomBar from './BottomBar'
+import FactBox from './FactBox';
 
 export default function Home() {
   return (
     <><div>Home</div><Box sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }}>
+      <FactBox/>
           <BottomBar sx={{ width: '100%' }} />
       </Box></>
   )
