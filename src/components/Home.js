d16367afd364932b54d4eb406d4d1973dc7e1438
timol085 <timol085@student.liu.se>
2023-04-06 11:53:46 +0200

    trying to merge fix

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,7 @@
 import { Box } from '@mui/material'
 import React from 'react'
 import BottomBar from './BottomBar'
-import FactBox from './FactBox';
-import TypeBox from './TypeBox';
+
 
 
 
@@ -10,8 +9,7 @@ import TypeBox from './TypeBox';
 export default function Home() {
   return (
     <><div>Home</div><Box sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }}>
-      <TypeBox value={1} type="money" />
-      <FactBox/>
+    
           <BottomBar sx={{ width: '100%' }} />
       </Box></>
   )
