d28c9293be5229eea352bcc5ff7e9590e9396c8b
Moa Burevik <93648713+Mosmelon@users.noreply.github.com>
2023-04-06 10:20:28 +0200

    topbar

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,8 +4,11 @@ import BottomBar from './BottomBar'
 
 export default function Home() {
   return (
-    <><div>Home</div><Box sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }}>
+    <>
+
+    <Box sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }}>
           <BottomBar sx={{ width: '100%' }} />
       </Box></>
+
   )
 }
--- src/components/Stat.js
+++ src/components/Stat.js
@@ -2,9 +2,12 @@ import { Box } from '@mui/material'
 import React from 'react'
 import BottomBar from './BottomBar'
 
+
 export default function Stat() {
   return (
-    <><div>Stat</div><Box sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }}>
+    <>
+
+    <Box sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }}>
           <BottomBar sx={{ width: '100%' }} />
       </Box></>
   )
